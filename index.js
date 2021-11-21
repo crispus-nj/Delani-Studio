9b634993b57c73821e4a29ca46060ff2a81a25b7
engineer237 <crispus.muthiora@student.moringaschool.com>
2021-11-21 14:51:18 +0300

    added classes to all titles in the what we do section. used the classes in jquery to add show effects.

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 $(document).ready(function () {
   $(".design-image").click(function () {
     $(".design-description").toggle();
+    $(".design-image").hide();
+    $(".design, .design-description").click(function () {
+      $(".design-image").show();
+      $(".design-description").hide();
+    });
   });
   $(".development-icon").click(function () {
     $(".development-description").toggle();
-    $(".development-icon").hide()
+    $(".development-icon").hide();
+    $(".development, .development-description").click(function () {
+      $(".development-description").hide();
+      $(".development-icon").show();
+    });
   });
   $(".product-icon").click(function () {
     $(".product-description").toggle();
+    $(".product-icon").hide();
+    $(".product, .product-description").click(function () {
+      $(".product-icon").show();
+      $(".product-description").hide();
+    });
   });
 });
