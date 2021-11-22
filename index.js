7e86cace5ba67ce94201d7b9098a759ee16e1af6
engineer237 <crispus.muthiora@student.moringaschool.com>
2021-11-22 17:05:34 +0300

    added form validation to the contact form

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// form validation
+$(document).ready(function () {
+  $("#form").submit(function (e) {
+    let userName = $("#name").val().trim();
+    let email = $("#email").val().trim();
+    let message = $("#message").val().trim();
+    e.preventDefault();
+    alert(`
+    Name: ${userName}.
+    Email: ${email}.
+    Message: ${message}.
+
+    Thank you for contact us.We will get back to you!!`);
+  });
+});
 
 // function for what we do section
 $(document).ready(function () {
@@ -27,29 +42,29 @@ $(document).ready(function () {
   });
 });
 // function to hover portfolio images
-$(document).ready(function(){
-  $('.img1').hover(function(){
-    $('.img1-description').slideToggle(1000)
-  })
-  $('.img2').hover(function(){
-    $('.img2-description').slideToggle(1000)
-  })
-  $('.img3').hover(function(){
-    $('.img3-description').slideToggle(1000)
-  })
-  $('.img4').hover(function(){
-    $('.img4-description').slideToggle(1000)
-  })
-  $('.img5').hover(function(){
-    $('.img5-description').slideToggle(1000)
-  })
-  $('.img6').hover(function(){
-    $('.img6-description').slideToggle(1000)
-  })
-  $('.img7').hover(function(){
-    $('.img7-description').slideToggle(1000)
-  })
-  $('.img8').hover(function(){
-    $('.img8-description').slideToggle(1000)
-  })
-})
+$(document).ready(function () {
+  $(".img1").hover(function () {
+    $(".img1-description").slideToggle(1000);
+  });
+  $(".img2").hover(function () {
+    $(".img2-description").slideToggle(1000);
+  });
+  $(".img3").hover(function () {
+    $(".img3-description").slideToggle(1000);
+  });
+  $(".img4").hover(function () {
+    $(".img4-description").slideToggle(1000);
+  });
+  $(".img5").hover(function () {
+    $(".img5-description").slideToggle(1000);
+  });
+  $(".img6").hover(function () {
+    $(".img6-description").slideToggle(1000);
+  });
+  $(".img7").hover(function () {
+    $(".img7-description").slideToggle(1000);
+  });
+  $(".img8").hover(function () {
+    $(".img8-description").slideToggle(1000);
+  });
+});
